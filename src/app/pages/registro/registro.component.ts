4ca7682b6f4a9a775942e859213219cdb489bfc7
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-10-29 17:59:56 -0600

    recordar

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../serices/auth.service';
 
@@ -11,23 +14,44 @@ import { AuthService } from '../../serices/auth.service';
 export class RegistroComponent implements OnInit {
   
   usuario:UsuarioModel;
-
-  constructor(private auth: AuthService) { }
+  recordarme = false;
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() { 
     this.usuario = new UsuarioModel();
   }
   onSubmit( form: NgForm){
     if(form.invalid){return;}
+
+    Swal.fire({
+      allowOutsideClick:false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+  Swal.showLoading();
     
     this.auth.nuevoUsusario(this.usuario)
     .subscribe(resp =>{
       
       console.log(resp);
+      Swal.close();
+
+      if (this.recordarme) {
+        localStorage.setItem('email', this.usuario.email);
+      }
+
+
+      this.router.navigateByUrl('/home');
       
       
     }, (err) =>{
         console.log(err.error.error.message);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al autenticar',
+          text: err.error.error.message
+        });
         
     });
 
