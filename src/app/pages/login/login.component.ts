a2481fbe330df297d382fc5d68a45f3345e585e5
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-10-28 20:19:16 -0600

    Guardar token en local

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
+import { AuthService } from '../../serices/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private auth:AuthService) { }
+  
+  usuario: UsuarioModel = new UsuarioModel();
+
+
+  ngOnInit() {
+  }
+
+  login(form: NgForm){
+
+    if( form.invalid){return;}
+    
+    this.auth.login(this.usuario)
+    .subscribe( resp =>{
+
+      console.log(resp);
+      
+
+    }, (err) =>{
+        
+      console.log(err.error.error.message);
+      
+
+
+    });
+    
+  }
+
+}
---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
+import { AuthService } from '../../serices/auth.service';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+  
+  usuario:UsuarioModel;
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() { 
+    this.usuario = new UsuarioModel();
+  }
+  onSubmit( form: NgForm){
+    if(form.invalid){return;}
+    
+    this.auth.nuevoUsusario(this.usuario)
+    .subscribe(resp =>{
+      
+      console.log(resp);
+      
+      
+    }, (err) =>{
+        console.log(err.error.error.message);
+        
+    });
+
+    }
+    
+    
+    
+  }
+
+
